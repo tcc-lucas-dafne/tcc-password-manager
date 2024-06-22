263578425ca256b95bb461976282520812f02e80
DafneM <moretti.dafne2@gmail.com>
2024-06-22 20:56:57 -0300

    fix: fix dast tests

--- scripts/save_report.sh
+++ scripts/save_report.sh
@@ -3,6 +3,7 @@
 github_workspace=$1
 repository_name=$2
 ref_name=$3
+report_name=$4
 
 git clone "https://x-access-token:$TOKEN_GIT@github.com/tcc-lucas-dafne/$NAME_REPO_GIT" $NAME_REPO_GIT
 cd $NAME_REPO_GIT
@@ -13,10 +14,10 @@ mkdir -p reports/$TOOL_NAME/$current_date
 
 export TZ=America/Sao_Paulo
 hour=$(date +'%d-%m-%Y_%H-%M-%S')
-mv $github_workspace/$REPORT_NAME.$REPORT_FORMAT $github_workspace/$REPORT_NAME_$hour.$REPORT_FORMAT
-cp $github_workspace/$REPORT_NAME_$hour.$REPORT_FORMAT reports/$TOOL_NAME/$current_date/
+mv $github_workspace/$report_name.$REPORT_FORMAT $github_workspace/$report_name_$hour.$REPORT_FORMAT
+cp $github_workspace/$report_name_$hour.$REPORT_FORMAT reports/$TOOL_NAME/$current_date/
 
-git add reports/$TOOL_NAME/$current_date/$REPORT_NAME_$hour.$REPORT_FORMAT
+git add reports/$TOOL_NAME/$current_date/$report_name_$hour.$REPORT_FORMAT
 git add .
 git commit -m "Adicionando relatório no repositório $repository_name $ref_name [skip ci]"
 git pull origin develop
