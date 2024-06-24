e454e13b494a2c9d9088faffc3a9905c68710562
Dafne Moretti <54643335+DafneM@users.noreply.github.com>
2024-06-23 23:53:09 -0300

    Create save_reports.sh

---
+++ scripts/save_reports.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+github_workspace=$1
+repository_name=$2
+ref_name=$3
+
+git clone "https://x-access-token:$TOKEN_GIT@github.com/tcc-lucas-dafne/$NAME_REPO_GIT" $NAME_REPO_GIT
+cd $NAME_REPO_GIT
+git checkout develop
+
+export TZ=America/Sao_Paulo
+current_date=$(date +%d-%m-%Y)
+mkdir -p reports/$TOOL_NAME/$current_date
+
+hour=$(date +'%d-%m-%Y_%H-%M-%S')
+mv $github_workspace/"${REPORT_NAME}.${REPORT_FORMAT}" $github_workspace/"${REPORT_NAME}_${hour}.${REPORT_FORMAT}"
+cp $github_workspace/"${REPORT_NAME}_${hour}.${REPORT_FORMAT}" reports/$TOOL_NAME/$current_date/
+
+git add reports/$TOOL_NAME/$current_date/"${REPORT_NAME}_${hour}.${REPORT_FORMAT}"
+git add .
+git commit -m "Adicionando relatório no repositório $repository_name $ref_name [skip ci]"
+git pull origin develop
+git push origin develop
