64d1c75495e88c891ed6e2aec9d96edcc9f9cc84
lcsAndrade <lucascandradex@protonmail.com>
2024-06-25 03:46:19 -0300

    fix: api url

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,1 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+const API_URL_HML = process.env.REACT_APP_API_URL_HML;
+const API_URL_PROD = process.env.REACT_APP_API_URL_PROD;
+
+export const API_URL = process.env.NODE_ENV === 'production' ? API_URL_PROD : API_URL_HML;
